270251a70cc92cca24bc96de603b4b76368f6538
bradenneal <bradenneal@31f12aec-1f03-11de-a108-cd2f117ce590>
2010-02-10 02:26:10 +0000

    added a module that can insert commands into the instruction pipeline
    git-svn-id: https://clustergl2.googlecode.com/svn/trunk@64 31f12aec-1f03-11de-a108-cd2f117ce590

---
+++ src/mod_insert.cpp
@@ -0,0 +1,69 @@
+/********************************************************
+	Headers
+********************************************************/
+
+#include "main.h"
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <GL/glu.h>
+
+
+/*********************************************************
+	Insertion Globals
+*********************************************************/
+
+Instruction mInst[100];
+//the number of instructions used
+int instCount = 0;
+//current instruction
+Instruction *mCurrInst = NULL;
+byte *mArgs = NULL;
+
+/*********************************************************
+	Interception module stuff
+*********************************************************/
+InsertModule::InsertModule(){
+	netBytes= 0;
+	netBytes2 = 0;
+	frames = 0;
+	init();
+}
+
+bool InsertModule::init(){
+	//create the list we are going to add to each frame	
+	//
+
+	LOG("InsertModule!\n");
+	return true;
+}
+
+bool InsertModule::process(list<Instruction> &list){
+	LOG("InsertModule: Processing!\n");
+
+	Instruction lastInstruction = list.back();
+	
+	if(lastInstruction.id == 1499) {//i.e. swap buffers
+
+		//remove swap buffers
+		list.pop_back();
+		
+		//add our instructions
+		for(int i=0;i<instCount;i++){
+			list.push_back(mInst[i]);
+		}
+		
+		//add swap buffers
+		list.push_back(lastInstruction);
+	}
+	
+	return true;
+}
+
+void InsertModule::reply(Instruction *instr, int i){
+	LOG("InsertModule::reply: Shouldn't happen!\n");
+}
+
+bool InsertModule::sync(){
+	return true;
+}
+
